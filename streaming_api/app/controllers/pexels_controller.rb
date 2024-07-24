0eb87344a7fba9e8ce69500cfa6d8013d6c5b244
andbri321 <andbri321@gmail.com>
2024-07-25 00:14:53 +0100

    [create] pexels controller

---
+++ streaming_api/app/controllers/pexels_controller.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class PexelsController < ApplicationController
+  before_action :call_pexel, only: %i[index show]
+
+  def index
+    if @response[:data]
+      render json: @response[:data]
+    else
+      render json: @response, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    if @response[:data]
+      render json: @response[:data]
+    else
+      render json: @response, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def call_pexel
+    @response = PexelService.call(params)
+  end
+end

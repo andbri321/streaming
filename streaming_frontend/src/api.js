1b85de15c4e1988b86a885ca48437eb0a02d8999
andbri321 <andbri321@gmail.com>
2024-07-25 09:45:08 +0100

    [create] core redux-toolkit and api.js

---
+++ streaming_frontend/src/api.js
@@ -0,0 +1,50 @@
+export const API_URL = 'http://localhost:3000'
+
+export function TOKEN_POST(body) {
+    return {
+        url: API_URL + '/login',
+        options: {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(body),
+        },
+    };
+}
+
+export function TOKEN_VALIDATE_POST(token) {
+    return {
+        url: API_URL + '/token_validation',
+        options: {
+            method: 'GET',
+            headers: {
+                Authorization: 'Bearer ' + token,
+            },
+        },
+    };
+}
+
+export function PEXEL_BY_ID_GET(id) {
+    return {
+        url: `${API_URL}/pexels/${id}`,
+        options: {
+            method: 'GET',
+            cache: 'no-store',
+            headers: {
+            },
+        },
+    };
+}
+
+export function PEXELS_GET(token, page, per_page,search) {
+    return {
+        url: `${API_URL}/pexels/?search=dog&&page=${page}&&per_page=${per_page}&&${search}`,
+        options: {
+            method: 'GET',
+            cache: 'no-store',
+            headers: {
+            },
+        },
+    };
+}
---
+++ streaming_frontend/src/store/pexel_by_id.js
@@ -0,0 +1,66 @@
+import { PEXEL_BY_ID_GET } from '../api';
+
+const FETCH_PEXEL_BY_ID_STARTED = 'pexel_by_id/fetchStarted';
+const FETCH_PEXEL_BY_ID_SUCCESS = 'pexel_by_id/fetchSuccess';
+const FETCH_PEXEL_BY_ID_ERROR = 'pexel_by_id/fetchError';
+
+const fetchPexelByIdStarted = () => ({
+    type: FETCH_PEXEL_BY_ID_STARTED
+})
+
+const fetchPexelByIdSuccess = (data) => ({
+    type: FETCH_PEXEL_BY_ID_SUCCESS,
+    payload: data
+})
+
+const fetchPexelByIdError = (error) => ({
+    type: FETCH_PEXEL_BY_ID_ERROR,
+    payload: error
+})
+
+const initialState = {
+    loading: false,
+    error: null,
+    data:null,
+}
+
+export default function user(state=initialState,action){
+    switch(action.type){
+        case FETCH_PEXEL_BY_ID_STARTED:
+            return {
+                ...state,
+                loading: true,
+                data: null,
+                error: null
+            };
+        case FETCH_PEXEL_BY_ID_SUCCESS:
+          return {
+                ...state,
+                loading: false,
+                data: action.payload,
+                error: null
+            };
+        case FETCH_PEXEL_BY_ID_ERROR:
+            return {
+                ...state,
+                loading: false,
+                error: action.payload,
+                data: null
+            };
+        default:
+            return state;
+    }
+}
+
+export const fetchPexelById = (token) => async (dispatch) =>{
+    try{
+        dispatch(fetchPexelByIdStarted());
+        const { url, options } = PEXEL_BY_ID_GET(token);
+        const response = await fetch(url, options);
+        const data = await response.json();
+        if(response.ok === false) throw new Error(data.message);
+        dispatch(fetchPexelByIdSuccess(data));
+    } catch (error) {
+        dispatch(fetchPexelByIdError(error.message));
+    }
+}

9b0036ac36ae6c84852b98b031c92583e48e40e0
andbri321 <andbri321@gmail.com>
2024-07-25 07:00:20 +0100

    [create] svg icons

--- streaming_frontend/src/components/Feed/FeedList.js
+++ streaming_frontend/src/components/Feed/FeedList.js
@@ -1,11 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Container from 'react-bootstrap/Container';
-// import { useDispatch, useSelector } from 'react-redux';
-// import FeedShowList from './FeedShowList';
-// import FeedPagination from './FeedPagination';
 import FeedGrid from './FeedGrid';
 import FeedShowList from './FeedShowList';
-// import { fetchUsers } from '../store/users';
 import FeedHeader from './FeedHeader';
 import FeedPagination from './FeedPagination';
 

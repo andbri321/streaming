ac37d088e89b7f3031b5802c24966091e116c35b
andbri321 <andbri321@gmail.com>
2024-07-25 09:49:37 +0100

    [update] component App, behavior added

--- streaming_frontend/src/App.js
+++ streaming_frontend/src/App.js
@@ -1,12 +1,35 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Feed from './components/Feed/Feed';
+import NotFound from './components/NotFound';
+import { BrowserRouter , Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className='bg-secondary'>
       <Header />
-      <Feed/>
+      <BrowserRouter>
+        <Routes>
+
+          <Route 
+            path="/"
+            element={
+              <Feed/>
+            }
+          />
+          <Route
+            path="movies/*"
+            element={
+              <Feed/>
+            }
+          />
+          <Route 
+            path="/login/*"
+            element={<NotFound />}
+          />
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
